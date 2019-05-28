acaab0346ed3c7f85978ba403b0b48067412b7d6
paulo.roberto <paulosix@hotmail.com>
2019-05-28 18:45:01 -0300

    Criação da listagem

--- contas/views.py
+++ contas/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Transacao
 import datetime
 
 def home(request):
-    now = datetime.datetime.now()
+    data = {}
+    data['transacoes'] = ['t1', 't2', 't3']
+
+    data['now'] = datetime.datetime.now()
     # html = "<html><body>It is now %s.</body></html>" % now
 
-    return render(request, 'contas/home.html')
+    return render(request, 'contas/home.html', data)
+
+def listagem(request):
+    data = {}
+    data['transacoes'] = Transacao.objects.all()
+
+    return render(request, 'contas/listagem.html', data)
