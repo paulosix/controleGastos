fa5c50ded3b3b1227f5d83e591702da94144745b
paulo.roberto <paulosix@hotmail.com>
2019-05-28 19:37:01 -0300

    Encerramento do CRUD

--- contas/views.py
+++ contas/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Transacao
 import datetime
+from .form import TransacaoForm
 
 def home(request):
     data = {}
@@ -16,3 +17,35 @@ def listagem(request):
     data['transacoes'] = Transacao.objects.all()
 
     return render(request, 'contas/listagem.html', data)
+
+def nova_transacao(request):
+    data = {}
+    form = TransacaoForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+    data['form'] = form
+    return render(request, 'contas/form.html', data)
+
+
+def update(request, pk):
+    data = {}
+    transacao = Transacao.objects.get(pk=pk)
+
+    form = TransacaoForm(request.POST or None, instance=transacao)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+    data['form'] = form
+    data['transacao'] = transacao
+    return render(request, 'contas/form.html', data)
+
+def delete(request, pk):
+    transacao = Transacao.objects.get(pk=pk)
+    transacao.delete()
+
+    return redirect('url_listagem')
--- controle_gastos/urls.py
+++ controle_gastos/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from contas.views import home, listagem
+from contas.views import home, listagem, nova_transacao, update, delete
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', listagem),
+    path('', listagem, name='url_listagem'),
+    path('nova/', nova_transacao, name='url_nova'),
+    path('update/<int:pk>/', update, name='url_update'),
+    path('delete/<int:pk>/', delete, name='url_delete'),
     path('home/', home)
 ]
